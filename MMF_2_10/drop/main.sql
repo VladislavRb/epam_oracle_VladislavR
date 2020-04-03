fd8565e420e8ce54b39372f8256ac9fd804e6752
VladislavRb <vlad.raba@mail.ru>
2020-04-03 09:32:25 +0300

    fixed: logs

--- MMF_2_10/drop/main.sql
+++ MMF_2_10/drop/main.sql
@@ -1,4 +1,7 @@
 SPOOL 'drop_schema.log'
+PROMPT dropping user
 @drop_user.sql &1
+
+PROMPT dropping tablespaces
 @drop_tablespace.sql &1
 SPOOL OFF
--- MMF_2_10/install/main.sql
+++ MMF_2_10/install/main.sql
@@ -1,5 +1,10 @@
 SPOOL 'install_schema.log'
+PROMPT creating tablespaces
 @create_tablespace.sql &1
+
+PROMPT creating user
 @create_user.sql &1 &2
+
+PROMPT executing grants script
 @grants.sql &1
 SPOOL OFF
